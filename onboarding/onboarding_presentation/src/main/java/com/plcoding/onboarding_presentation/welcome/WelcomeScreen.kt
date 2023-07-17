46a4c002338de9c62259e29c441041be2fdcc4b2
TSS-22 <andpartyandbullshit@protonmail.com>
2023-07-17 20:24:09 +1200

    part 4 - DONE

--- onboarding/onboarding_presentation/src/main/java/com/plcoding/onboarding_presentation/welcome/WelcomeScreen.kt
+++ onboarding/onboarding_presentation/src/main/java/com/plcoding/onboarding_presentation/welcome/WelcomeScreen.kt
@@ -13,13 +13,16 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import com.plcoding.calorytracker.R
+import com.plcoding.core.navigation.Route
+import com.plcoding.core.util.UiEvent
 import com.plcoding.core_ui.LocalSpacing
 import com.plcoding.onboarding_presentation.components.ActionButton
 
 @Composable
-fun WelcomeScreen(){
+fun WelcomeScreen(
+    onNavigate: (UiEvent.Navigate) -> Unit
+){
 
     val spacing = LocalSpacing.current
 
@@ -38,7 +41,7 @@ fun WelcomeScreen(){
         Spacer(modifier = Modifier.height(spacing.spaceMedium))
         ActionButton(
             text = stringResource(id = R.string.next),
-            onClick = { /*TODO*/ },
+            onClick = { onNavigate(UiEvent.Navigate(Route.AGE)) },
             modifier = Modifier.align(Alignment.CenterHorizontally))
     }
 }
